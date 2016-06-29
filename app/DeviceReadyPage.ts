8d7c9d9f9c3276426a66c7dd312f31fd9bca046a
ridomin <rmpablos@hotmail.com>
2016-06-29 13:22:20 -0700

    initial commit

---
+++ app/DeviceReadyPage.ts
@@ -0,0 +1,17 @@
+﻿export class DeviceReadyPage {
+
+    constructor() {
+        let mainDiv: HTMLElement = document.createElement("div")
+        mainDiv.className = "app"
+        mainDiv.innerHTML = '<h1>Tools for Apache Cordova </h1><div id= "deviceready" class="blink" ><p class="event listening" > Connecting to Device< /p><p class="event received">Device is Ready</p></div>'
+        document.body.appendChild(mainDiv)
+    }
+
+    start() {
+        let parentElement : HTMLElement= document.getElementById('deviceready');
+        let listeningElement :Element= parentElement.querySelector('.listening');
+        let receivedElement :Element= parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    }
+}
---
+++ app/app.ts
@@ -0,0 +1,11 @@
+﻿import {DeviceReadyPage} from "./DeviceReadyPage"
+
+export class App {
+    init() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    }
+
+    onDeviceReady() {
+        new DeviceReadyPage().start()
+    }
+}
