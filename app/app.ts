1727cff2e7aa4fe16456f820df1dcd68db3326b0
Rido Min <ridomin@live.com>
2016-07-13 02:57:40 -0700

    refactor deviceReady

--- app/app.ts
+++ app/app.ts
@@ -7,10 +7,6 @@ export class App {
     constructor() {
         this.deviceReadyPage = new DeviceReadyPage()
         this.deviceReadyPage.render(document.body);
-        document.addEventListener("deviceready", () => this.onDeviceReady(), false)
-    }
-
-    onDeviceReady() {
-        this.deviceReadyPage.onDeviceReady();
+        document.addEventListener("deviceready", () => this.deviceReadyPage.onDeviceReady(), false)
     }
 }
